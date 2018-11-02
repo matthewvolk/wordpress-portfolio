f2d2e72c2a31bae2e178415484dfe62d66056b52
matthewvolk <volkmattj@gmail.com>
2018-11-01 19:48:55 -0700

    update hero text

--- front-page.php
+++ front-page.php
@@ -3,7 +3,7 @@
 <div class="jumbotron jumbotron-fluid jumbotron-front-page pt-1 pb-4 bg-black">
   <div class="container">
     <h1 class="display-5">My name's Matt.</h1>
-    <p class="lead">I build websites using the following technologies:</p>
+    <p class="lead">I'm a web developer using the following technologies:</p>
     <i class="devicon-javascript-plain px-2 py-2"></i>
     <i class="devicon-nodejs-plain px-2 py-2"></i>
     <i class="devicon-php-plain px-2 py-2"></i>
